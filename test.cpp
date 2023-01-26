a2e4772f1fd3d76d16900452e760b0818d412d4c
biga6132 <biga6132@colorado.edu>
2023-01-26 20:14:08 +0000

    doing things

--- test.cpp
+++ test.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
-#include CATCH_CONFIG_RUNNER
 #include "catch.hpp"
-#include "function_to_implement.cpp"
-
-int main(){
-    
-}
+#define CATCH_CONFIG_MAIN
+#include "functions_to_implement.cpp"
 
 TEST_CASE( "Sum of vector are computed", "[sum]" ) {
     std::vector<int> v1 = {1, 2, 3};
     std::vector<int> v2 = {2, 3, 4};
     std::vector<int> v3 = {3, 4, 5};
-    std::vector<int> v4 = {324, 923, 5432}
+    std::vector<int> v4 = {324, 923, 5432};
     REQUIRE( Sum(v1) == 6 );
     REQUIRE( Sum(v2) == 9 );
     REQUIRE( Sum(v3) == 12 );
@@ -22,7 +18,7 @@ TEST_CASE( "Product of vector are computed", "[product]" ) {
     std::vector<int> v1 = {1, 2, 3};
     std::vector<int> v2 = {2, 3, 4};
     std::vector<int> v3 = {3, 4, 5};
-    std::vector<int> v4 = {12, 37, 166}
+    std::vector<int> v4 = {12, 37, 166};
     REQUIRE( Product(v1) == 6 );
     REQUIRE( Product(v2) == 9 );
     REQUIRE( Product(v3) == 12 );
@@ -33,11 +29,16 @@ TEST_CASE( "VectorPlusN should add a value to each element", "[vectorPlusN]" ) {
     std::vector<int> v1 = {1, 2, 3};
     std::vector<int> v2 = {2, 3, 4};
     std::vector<int> v3 = {3, 4, 5};
-    std::vector<int> v4 = {12, 37, 166}
-    REQUIRE( VectorPlusN(v1, 2) == {3, 4, 5} );
-    REQUIRE( VectorPlusN(v2, 4) == {6, 7, 8} );
-    REQUIRE( VectorPlusN(v3, 16) == {19, 20, 21} );
-    REQUIRE( VectorPlusN(v4, 345) == {357, 382, 511} );
+    std::vector<int> v4 = {12, 37, 166};
+    //to check
+    std::vector<int> t1 = {3, 4, 5};
+    std::vector<int> t2 = {6, 7, 8};
+    std::vector<int> t3 = {19, 20, 21};
+    std::vector<int> t4 = {357, 382, 511};
+    REQUIRE( VectorPlusN(v1, 2) == t1 );
+    REQUIRE( VectorPlusN(v2, 4) == t2 );
+    REQUIRE( VectorPlusN(v3, 16) == t3 );
+    REQUIRE( VectorPlusN(v4, 345) == t4 );
 }
 
 
